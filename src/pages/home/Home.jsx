982876d20e3bb731cce2d610dc3ca7c2aba78408
agityaupadhyay9450445597-cyber <agityaupadhyay9450445597@gmail.com>
2026-01-10 09:58:19 +0530

    Updated home page background and text changes with Tailwind config

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,21 @@
-export default function Home(){
-  return <h1>Home Page – Under Construction</h1>
+import LandingSection from './homeComponents/landingSection.jsx';
+import ImageGallery from './homeComponents/imageGallery.jsx';
+import Sponsors from './homeComponents/sponsors.jsx';
+import Timeline from './homeComponents/timeline.jsx';
+import About from './homeComponents/abouthome.jsx';
+import HomeThreeBackground from '../../components/HomeThreeBackground.jsx';
+
+function Home() {
+    return (
+        <>
+            <HomeThreeBackground />
+            <LandingSection />
+            <ImageGallery />
+            <About/>
+            <Timeline/>
+            <Sponsors/>
+        </>
+    );
 }
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx,vue}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				vermin: ['VerminVibes'],
+				novega: ['Novega'],
+				knightWarrior: ['KnightWarrior'],
+				fastLines: ['FastLines'],
+				crossFly: ['CrossFly'],
+				speedPixel: ['speedPixel'],
+				speedPixelItalic: ['speedPixelItalic'],
+				brave81: ['Brave81']
+			}
+		},
+	},
+	plugins: [],
+};
